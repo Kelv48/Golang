2a7635055b0fef8385cf9eb014b981ae92f54929
Kelvin <kelvinosagie05@gmail.com>
2025-03-24 14:58:43 +0000

    Added in the toString function that takes a deck and turns it into a string

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type deck []string
 
@@ -30,4 +33,9 @@ func (d deck) print() {
 func deal(d deck, handsize int) (deck, deck) {
 	// Deals out cards from the deck
 	return d[:handsize], d[handsize:]
+}
+
+func (d deck) toString() string {
+	// Takes in a deck and returns a string
+	return strings.Join([]string(d), ",")
 }
