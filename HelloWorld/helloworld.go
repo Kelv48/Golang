25296bfa7aac97abc88c17e93082d9e6290f2236
Kelvin <kelvinosagie05@gmail.com>
2025-03-24 13:48:31 +0000

    initial commit

---
+++ HelloWorld/helloworld.go
@@ -0,0 +1,18 @@
+package main
+
+// Go run and the file we want to execute
+
+// Go build (compiles a bunch of go source code files)
+// Go run (compiles and executes a bunch of go source code)
+
+// Go fmt auto formats all the files
+
+// Go install and go get are responsible for dependecies
+
+// Go test is used for running any test cases in our directories
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
