901f6b349e2d4d4ee2d7d821650a3613d5ac931f
Kelvin <kelvinosagie05@gmail.com>
2025-03-24 15:04:43 +0000

    Added in the saveToFile function

--- cards/deck.go
+++ cards/deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -38,4 +39,8 @@ func deal(d deck, handsize int) (deck, deck) {
 func (d deck) toString() string {
 	// Takes in a deck and returns a string
 	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
