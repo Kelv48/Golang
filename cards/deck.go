a098f2f2393d607d92122d3406fe55043d3b7d59
Kelvin <kelvinosagie05@gmail.com>
2025-03-24 14:15:52 +0000

    Moved deck creation to deck.go

--- cards/deck.go
+++ cards/deck.go
@@ -4,11 +4,28 @@ import "fmt"
 
 // Create a new type of 'deck'
 // which is a slice of strings
-
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
+	// Log out the contents of a deck of cards
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
+
+// New deck create and return a list of playing cards
